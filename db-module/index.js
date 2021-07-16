0b87eb765338877b612f8ae16130e266bbc50519
alan <alanarostegui@hotmail.com>
2021-07-16 12:31:06 -0500

    introduccion a pruebas unitarias con Ava.js

--- db-module/index.js
+++ db-module/index.js
@@ -3,8 +3,21 @@
 const setupDatabase = require("./lib/db");
 const setupAgentModel = require("./models/agent");
 const setupMetricModel = require("./models/metric");
+const defaults = require("defaults");
 
 module.exports = async function (config) {
+  config = defaults(config, {
+    dialect: "sqlite",
+    pool: {
+      max: 10,
+      min: 0,
+      idle: 10000,
+    },
+    query: {
+      raw: true,
+    },
+  });
+
   // Define models and relationships
   const sequelize = setupDatabase(config);
   const AgentModel = setupAgentModel(config);
@@ -16,9 +29,9 @@ module.exports = async function (config) {
   // Try connection with DB
   await sequelize.authenticate();
 
-    // exec sync and force to erase db if setup existe
+  // exec sync and force to erase db if setup existe
   if (config.setup) {
-    await sequelize.sync({ force: true})
+    await sequelize.sync({ force: true });
   }
 
   const Agent = {};
