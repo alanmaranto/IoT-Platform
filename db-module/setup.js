bdc081a09250a0d1e2ce976f84b13ddb75eb358b
alan <alanarostegui@hotmail.com>
2021-07-15 22:08:29 -0500

    creando una advertencia sobre el borrado de base de datos

--- db-module/setup.js
+++ db-module/setup.js
@@ -1,9 +1,25 @@
 "use strict";
 
 const debug = require("debug")("iotverse:db:setup");
+const inquirer = require("inquirer");
+const chalk = require("chalk");
 const db = require("./");
 
+const prompt = inquirer.createPromptModule();
+
 async function setup() {
+  const answer = await prompt([
+    {
+      type: "confirm",
+      name: "setup",
+      message: "This will destroy your database, are you sure?",
+    },
+  ]);
+
+  if (!answer.setup) {
+    return console.log("Nothing happened :)");
+  }
+
   const config = {
     database: process.env.DB_NAME || "iotverse",
     username: process.env.DB_USER || "iot_user",
@@ -20,7 +36,7 @@ async function setup() {
 }
 
 function handleFatalError(error) {
-  console.error(error.message);
+  console.error(`${chalk.red("[fatal error]")} ${error.message}`);
   console.error(error.stack);
   process.exit(1);
 }
