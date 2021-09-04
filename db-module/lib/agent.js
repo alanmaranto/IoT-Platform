153ae666ce4978a311ed447b9494e060a5bfdd90
alan <alanarostegui@hotmail.com>
2021-09-04 15:11:38 -0500

    test(create or update): add update test

--- db-module/lib/agent.js
+++ db-module/lib/agent.js
@@ -4,7 +4,28 @@ module.exports = function setupAgent(AgentModel) {
   function findById(id) {
     return AgentModel.findById(id);
   }
+  async function createOrUpdate(agent) {
+    const condition = {
+      where: {
+        uuid: agent.uuid,
+      },
+    };
+
+    const existingAgent = await AgentModel.findOne(condition);
+
+    if (existingAgent) {
+      const updated = await AgentModel.update(agent, condition);
+      if (updated) {
+        return AgentModel.findOne(condition);
+      }
+      return existingAgent;
+    }
+
+    const result = await AgentModel.create(agent);
+    return result.toJSON();
+  }
   return {
     findById,
+    createOrUpdate,
   };
 };
--- db-module/tests/agent-tests.js
+++ db-module/tests/agent-tests.js
@@ -16,9 +16,16 @@ let MetricStub = {
 
 let single = Object.assign({}, agentFixtures.single);
 let id = 1;
+let uuid = "yyy-yyy-yyy";
 let AgentStub = null;
 let db = null;
 let sandbox = null;
+
+let uuidArgs = {
+  where: {
+    uuid,
+  },
+};
 
 test.beforeEach(async () => {
   sandbox = sinon.createSandbox();
@@ -26,8 +33,22 @@ test.beforeEach(async () => {
     hasMany: sinon.spy(),
   };
 
+  //Model find one Stub
+  AgentStub.findOne = sandbox.stub();
+  AgentStub.findOne
+    .withArgs(uuidArgs)
+    .returns(Promise.resolve(agentFixtures.byUuid(uuid)));
+
+  // Model update Stub
+  AgentStub.update = sandbox.stub();
+  AgentStub.update.withArgs(single, uuidArgs).returns(Promise.resolve(single));
+
+  // Model create Stub
+  AgentStub.create = sandbox.stub();
+  AgentStub.create.withArgs(single).returns(Promise.resolve(single));
+
   // Model findByID Stub
-  AgentStub.findById = sinon.stub();
+  AgentStub.findById = sandbox.stub();
   AgentStub.findById
     .withArgs(id)
     .returns(Promise.resolve(agentFixtures.byId(id)));
@@ -63,9 +84,28 @@ test.serial("Setup", (t) => {
 test.serial("AgentFindById", async (t) => {
   let agent = await db.Agent.findById(id);
 
-  t.true(AgentStub.findById.called, 'findById should be called on a model')
-  t.true(AgentStub.findById.calledOnce, 'findById should be called once')
-  t.true(AgentStub.findById.calledWith(id), 'findById should be called with specified id')
+  t.true(AgentStub.findById.called, "findById should be called on a model");
+  t.true(AgentStub.findById.calledOnce, "findById should be called once");
+  t.true(
+    AgentStub.findById.calledWith(id),
+    "findById should be called with specified id"
+  );
 
   t.deepEqual(agent, agentFixtures.byId(id), "should be the same");
 });
+
+test.serial("Agent#createOrUpdate - when user exists", async (t) => {
+  let agent = await db.Agent.createOrUpdate(single);
+
+  t.true(AgentStub.findOne.called, 'findOne should be called on model')
+  t.true(AgentStub.findOne.calledTwice, 'findOne should be called twice')
+  t.true(AgentStub.update.calledOnce, 'update should be called once')
+
+  t.deepEqual(agent, single, "agent should be the same");
+});
+
+/* test.serial("Agent#createOrUpdate - when user does not exist", async (t) => {
+  let agent = await db.Agent.createOrUpdate(single);
+
+  t.deepEqual(agent, single, "should create agent");
+}); */
