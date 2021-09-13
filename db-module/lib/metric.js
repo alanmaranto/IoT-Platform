8a40c34331a83e03846194f53b430dd9a2d6b213
alan <alanarostegui@hotmail.com>
2021-09-13 16:10:17 -0500

    implementacion del servicio metric

---
+++ db-module/lib/metric.js
@@ -0,0 +1,61 @@
+"use strict";
+
+module.exports = function setupMetric(MetricModel, AgentModel) {
+  async function findByAgentUuid(uuid) {
+    const condition = {
+      attributes: ["type"],
+      group: ["type"],
+      include: [
+        {
+          attributes: [],
+          model: AgentModel,
+          where: {
+            uuid,
+          },
+        },
+      ],
+      raw: true,
+    };
+    return MetricModel.findAll(condition);
+  }
+
+  async function create(uuid, metric) {
+    const agent = await AgentModel.findOne({
+      where: { uuid },
+    });
+
+    if (agent) {
+      // metric.agentId = agent.id
+      Object.assign(metric, { agent: agent.id });
+      const result = await MetricModel.create(metric);
+      return result.toJSON();
+    }
+  }
+
+  async function findByTypeAgentUid(type, uuid) {
+    const condition = {
+      attributes: ["id", "type", "value", "createdAt"],
+      where: {
+        type,
+      },
+      limit: 20,
+      order: [["createdAt", "DESC"]],
+      include: [
+        {
+          attributes: [],
+          model: AgentModel,
+          where: {
+            uuid,
+          },
+        },
+      ],
+      raw: true,
+    };
+    return MetricModel.findAll(condition);
+  }
+  return {
+    create,
+    findByAgentUuid,
+    findByTypeAgentUid,
+  };
+};
